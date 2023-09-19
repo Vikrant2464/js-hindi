e4b2e7cd14536a6ea888ea2bd42d755964c3dec6
vikrant singh <vikrantsingh2464@gmail.com>
2023-09-19 19:36:35 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "vik@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+/*
+prefer not ot use block
+because of issue in block scope and functional scope.
+*/
+
+ // accountId = 2 //not allowed
+
+
+
+ accountEmail = "hc@gmai.com"
+ accountPassword = "212121"
+ accountCity = "Bengaluru"
+
+ console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+ 
