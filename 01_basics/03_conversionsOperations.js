a49edd4388de76ed9e84b25e322aa8374ed3c30b
vikrant singh <vikrantsingh2464@gmail.com>
2023-09-19 22:10:33 +0530

    conversion

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,36 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+let score = "vikrant"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1  //true
+// let isLoggedIn = 0  //flase
+let isLoggedIn = ""  //false
+// let isLoggedIn = "vikrant"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "vikrant" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof (stringNumber));
