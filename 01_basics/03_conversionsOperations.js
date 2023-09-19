3f308b4c75cad821ffd5de2e1ac6e54c1bf3bc3b
vikrant singh <vikrantsingh2464@gmail.com>
2023-09-20 05:29:35 +0530

    operations

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -53,8 +53,26 @@ let str1 = "hello"
 let str2 = "vikrant"
 
 let str3 = str1 + str2
-console.log(str3);
+//console.log(str3);
 
-console.log("1" + "2");
-console.log(1 + "2");
-console.log("1" + "2");
+// console.log("1" + "2");  //12
+// console.log(1 + "2");    //12
+// console.log("1" + "2");  //12
+// console.log("1" + 2 + 2);  //122
+// console.log(1 + 2 + "2");  //32
+
+//console.log(+true); //1
+// console.log(true+); //error
+//console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2= num3 =2+2
+
+let gameCounter = 100
+//gameCounter++   // post increment ++  //101
+// ++gameCounter     // pre increment --
+ //101
+//--gameCounter     // pre decrement --
+gameCounter--
+console.log(gameCounter); 
