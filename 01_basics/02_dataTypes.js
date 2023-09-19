3ef9ab00d69df1cd502f8e2d954568194b9eb157
vikrant singh <vikrantsingh2464@gmail.com>
2023-09-19 20:03:09 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+// "use strict"; //treat all js code as newer version
+
+// alert( 3+3) //we are using nodejs not browser
+
+//code readability should be high
+
+console.log("vikrant")
+
+
+let name = 'vikrant'
+let age = 23
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+//symbol = unique
+
+// object
+
+console.log(typeof "vikrant");  //string
+console.log(typeof age);  //number
+console.log(typeof null);  // object
+console.log(typeof undefined);  //undefined 
