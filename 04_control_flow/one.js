cef22f36006c92f830c77b67aed9b485e11e1f17
vikrant <vikrantsingh2464@gmail.com>
2023-10-02 08:38:25 +0530

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,58 @@
+// if
+const isUserloggedIn = true
+
+if (2 === 2){
+  //  console.log("executed");
+}
+
+// const temperature = 41
+
+
+// if (temperature === 40) {
+//     console.log("less than 50");
+// }else {
+//     console.log("temperature is greater than 50");
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// const score = 200
+
+// if(score > 100)
+// {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+
+ const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500){
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("greater than 900");
+// }
+
+
+
+const UserloggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if(UserloggedIn && debitCard) {
+//     console.log("allow to buy course");
+// }
+
+if(loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
